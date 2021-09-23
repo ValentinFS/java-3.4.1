ea5c195cd70c562082a7583a9496f9faa10ed769
Valentin Fadeev <valentinfs@yandex.ru>
2021-09-23 23:14:33 +0300

    Deviding manager for Manager and Repository

--- src/main/java/ru/netology/AfishaManager.java
+++ src/main/java/ru/netology/AfishaManager.java
@@ -2,46 +2,29 @@ package ru.netology;
 
 public class AfishaManager {
 
-    public static void main(String[] args) {
-        MovieItems movie1 = new MovieItems(1, "Image", "Бладшот", "Боевик");
-        MovieItems movie2 = new MovieItems(2, "Image", "Вперед", "Мультфильм");
-        MovieItems movie3 = new MovieItems(3, "Image", "Отель Белград", "Комедия");
-        MovieItems movie4 = new MovieItems(4, "Image", "Джентельмены", "Боевик");
-        MovieItems movie5 = new MovieItems(5, "Image", "Человек-невидимка", "Ужасы");
-        MovieItems movie6 = new MovieItems(6, "Image", "Тролли. Мировой тур", "Мультфильм");
-        MovieItems movie7 = new MovieItems(7, "Image", "Номер один", "Комедия");
-        MovieItems movie8 = new MovieItems(8, "Image", "Босс-молокосос", "Мультфильм");
-        MovieItems movie9 = new MovieItems(9, "Image", "Травиата", "Драма");
-        MovieItems movie10 = new MovieItems(10, "Image", "Холостяк на свадьбе", "Комедия");
-        MovieItems movie11 = new MovieItems(11, "Image", "Небесная команда", "Драма");
-        MovieItems movie12 = new MovieItems(12, "Image", "Дюна", "Фантастика");
-        MovieItems movie13 = new MovieItems(13, "Image", "Литл Гром", "Мультфильм");
-        MovieItems movie14 = new MovieItems(14, "Image", "Отчаянные аферистки", "Комедия");
-        MovieItems movie15 = new MovieItems(15, "Image", "Семья года", "Комедия");
+    private AfishaRepository repository;
 
-        MovieManager manager = new MovieManager();
-        // MovieManager manager = new MovieManager(5);
-
-        manager.add(movie1);
-        manager.add(movie2);
-        manager.add(movie3);
-        manager.add(movie4);
-        manager.add(movie5);
-        manager.add(movie6);
-        manager.add(movie7);
-        manager.add(movie8);
-        manager.add(movie9);
-        manager.add(movie10);
-        manager.add(movie11);
-        manager.add(movie12);
-        manager.add(movie13);
-        manager.add(movie14);
-        manager.add(movie15);
-
-        manager.getAll();
-
-        manager.removeById(2);
-
+    public AfishaManager(AfishaRepository repository){
+        this.repository=repository;
     }
+
+    public void add(MovieItems item){
+        repository.save(item);
+    }
+
+    public MovieItems[] getAll(){
+        MovieItems[] items = repository.findAll();
+        MovieItems[] result = new MovieItems[items.length];
+        for (int i=0; i<result.length; i++) {
+            result[i]=items[result.length-i-1];
+        }
+        return result;
+    }
+
+    public void removeById(int id){
+        repository.removeById(id);
+    }
+
+
 
 }
