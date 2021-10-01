4c77a4dd469e207ecb9d64c7cb0d8aaeceb32647
Valentin Fadeev <valentinfs@yandex.ru>
2021-10-01 22:31:03 +0300

    Class main deleted and code reformated

--- src/main/java/ru/netology/AfishaManager.java
+++ src/main/java/ru/netology/AfishaManager.java
@@ -4,27 +4,26 @@ public class AfishaManager {
 
     private AfishaRepository repository;
 
-    public AfishaManager(AfishaRepository repository){
-        this.repository=repository;
+    public AfishaManager(AfishaRepository repository) {
+        this.repository = repository;
     }
 
-    public void add(MovieItems item){
+    public void add(MovieItems item) {
         repository.save(item);
     }
 
-    public MovieItems[] getAll(){
+    public MovieItems[] getAll() {
         MovieItems[] items = repository.findAll();
         MovieItems[] result = new MovieItems[items.length];
-        for (int i=0; i<result.length; i++) {
-            result[i]=items[result.length-i-1];
+        for (int i = 0; i < result.length; i++) {
+            result[i] = items[result.length - i - 1];
         }
         return result;
     }
 
-    public void removeById(int id){
+    public void removeById(int id) {
         repository.removeById(id);
     }
-
 
 
 }
